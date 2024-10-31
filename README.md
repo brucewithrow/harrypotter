7893cd87127b0708db99dfca9e4a61dbd06abc4d
Bruce Withrow <bruce.withrow@gmail.com>
2024-10-30 19:03:52 -0500

    First release of Random Harry Potter Info

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Random Harry Potter Information
+
+This app will query random information about the Harry Potter series using the PotterAPI.
+
+## Requirements
+
+1. Node.js installed locally
+
+## Installation
+
+1. Clone the git repository
+
+2. Install required dependencies
+        `$> npm i`
+
+3. Update node packages (optional)
+        `$> npm audit fix`
+
+## Run the app
+
+Open your browser and navigate to `localhost:3000`
+
+
---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import ejs from 'ejs'
+import axios from 'axios'
+
+const app = express();
+const PORT = 3000;
+
+const API_URL = 'https://potterapi-fedeperin.vercel.app/en'
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        title: staticPotterInfo.title, description: staticPotterInfo.description
+    });
+});
+
+app.get('/book', async (req, res) => {
+    try {
+        const result = await axios.get(`${API_URL}/books/random`);
+        res.render('index.ejs', { title: result.data.title, description: result.data.description, cover: result.data.cover } );
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.get('/character', async (req, res) => {
+    try {
+        const result = await axios.get(`${API_URL}/characters/random`);
+        res.render('index.ejs', { title: result.data.fullName, nickname: result.data.nickname, house: result.data.hogwartsHouse } );
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.get('/house', async (req, res) => {
+    try {
+        const result = await axios.get(`${API_URL}/houses/random`);
+        console.log(result);
+        res.render('index.ejs', { title: result.data.house, founder: result.data.founder, colors: result.data.colors, animal: result.data.animal } );
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.get('/spell', async (req, res) => {
+    try {
+        const result = await axios.get(`${API_URL}/spells/random`);
+        res.render('index.ejs', { title: result.data.spell, use: result.data.use} );
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port ${PORT}.`)
+});
+
+const staticPotterInfo = {
+    title: "Random information from the Harry Potter series",
+    description: "This app queries the PotterAPI to gather random information about various categories in the world of Harry Potter. You can choose from a random book, character, house, or spell by using the buttons below."
+};
