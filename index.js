5ca0e401be571210f5fed89aa2a87d8299aa985b
Bruce Withrow <bruce.withrow@gmail.com>
2024-10-30 19:35:47 -0500

    Updated comments in index.js and index.ejs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 const app = express();
 const PORT = 3000;
 
+// variable for the API...the endpoints will be added in each route
 const API_URL = 'https://potterapi-fedeperin.vercel.app/en'
 
 app.use(express.static('public'));
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
     });
 });
 
+// retrieve a random book with description and cover art
 app.get('/book', async (req, res) => {
     try {
         const result = await axios.get(`${API_URL}/books/random`);
@@ -25,6 +27,7 @@ app.get('/book', async (req, res) => {
     }
 });
 
+// retrieve a random character with name, nickname, and their house
 app.get('/character', async (req, res) => {
     try {
         const result = await axios.get(`${API_URL}/characters/random`);
@@ -35,6 +38,7 @@ app.get('/character', async (req, res) => {
     }
 });
 
+// retrieve a random house with house name, founder name, colors and animal
 app.get('/house', async (req, res) => {
     try {
         const result = await axios.get(`${API_URL}/houses/random`);
@@ -46,6 +50,7 @@ app.get('/house', async (req, res) => {
     }
 });
 
+// retrieve a random spell and its use
 app.get('/spell', async (req, res) => {
     try {
         const result = await axios.get(`${API_URL}/spells/random`);
@@ -60,6 +65,7 @@ app.listen(3000, () => {
     console.log(`Server is listening on port ${PORT}.`)
 });
 
+// This information displays when the user loads the page, before any buttons are clicked.
 const staticPotterInfo = {
     title: "Random information from the Harry Potter series",
     description: "This app queries the PotterAPI to gather random information about various categories in the world of Harry Potter. You can choose from a random book, character, house, or spell by using the buttons below."
